e1b091163af05c8e92c9688bb73d9bd5b2fba88c
axt <axt@load.hu>
2016-11-02 01:15:34 +0100

    Upgraded version to 0.0.2 because of package misnaming

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bingraphvis',
-    version='0.0.1',
+    version='0.0.2',
     packages=['bingraphvis', 'bingraphvis.angr', 'bingraphvis.angr.x86'],
     install_requires=[
         'pydot',
