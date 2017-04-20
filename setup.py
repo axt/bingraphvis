1324a50e8f868430fe2ef19c573eda984621df0c
axt <axt@load.hu>
2017-04-20 19:12:39 +0200

    Fix setup.py (#5)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='bingraphvis',
     version='0.0.2',
-    packages=['bingraphvis', 'bingraphvis.angr', 'bingraphvis.angr.x86'],
+    packages=['bingraphvis', 'bingraphvis.angr', 'bingraphvis.angr.x86', 'bingraphvis.angr.arm'],
     install_requires=[
         'pydot',
         'networkx'
