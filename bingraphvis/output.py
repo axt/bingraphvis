1e89905f09cd8b48611c73c0a5ed4bf2784c0cf7
axt <axt@load.hu>
2017-05-08 01:56:43 +0200

    Add bg-color to output

--- bingraphvis/output.py
+++ bingraphvis/output.py
@@ -59,7 +59,7 @@ class DotOutput(Output):
     
     def render_cell(self, key, data):
         if data != None and data['content'] != None and data['content'].strip() != '':
-            ret = '<TD '+ ('ALIGN="'+data['align']+'"' if 'align' in data else '' )+'>'
+            ret = '<TD '+ ('bgcolor="'+data['bgcolor']+'" ' if 'bgcolor' in data else '') + ('ALIGN="'+data['align']+'"' if 'align' in data else '' )+'>'
             if 'color' in data:
                 ret += '<FONT COLOR="'+data['color']+'">'
             if 'style' in data:
