b58bc5cd4aa81ba83c5ff3ca4c117441c52b255e
axt <axt@load.hu>
2017-07-21 10:20:33 +0200

    Add filter support to ColorNodes annotator

--- bingraphvis/annotator.py
+++ bingraphvis/annotator.py
@@ -1,14 +1,16 @@
 from .base import *
 
 class ColorNodes(NodeAnnotator):
-    def __init__(self, nodes, fillcolor=None, color=None):
+    def __init__(self, nodes=None, fillcolor=None, color=None, filter=None):
         super(ColorNodes, self).__init__()
         self.nodes = nodes
         self.fillcolor = fillcolor
         self.color = color
-    
+        self.filter = filter
+
+
     def annotate_node(self, node):
-        if node.obj in self.nodes:
+        if (self.nodes and node.obj in self.nodes) or (self.filter and self.filter(node)):
             node.style = 'filled'
             if self.fillcolor:
                 node.fillcolor = self.fillcolor
